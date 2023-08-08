7e3fbc172a8e587187436cea41c5a24af78ab45d
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-08 10:21:58 +0300

    Task solved

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int answer = first > second ? first: second;
+        System.out.println(answer);
     }
 }
+
+//    Rewrite Greatest number printer task from module 4.1 Task # 2 using rewrite some task with ternary operator:
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        System.out.println(first > second && second > third ? "1. Greatest from 3": "1. Not greatest from 3");
     }
 }
+
+//Rewrite "1. Greatest from 3." from this task using ternary operator
