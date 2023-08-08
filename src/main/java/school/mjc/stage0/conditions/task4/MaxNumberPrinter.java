04882cf7f5d4715d4c97518d6593184a409673e7
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-08 17:55:49 +0300

    Should i remove if statement even if its commented out?

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -4,14 +4,6 @@ public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
         int answer = (first >= second) ? ((first >= third) ? first : third) : ((second >= third) ? second : third);
         System.out.println(answer);
-
-//        if (first >= second && first >= third) {
-//            System.out.println(first);
-//        } else if (second >= third) {
-//            System.out.println(second);
-//        } else {
-//            System.out.println(third);
-//        }
     }
 }
 
