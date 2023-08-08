ff442dc1a635fae8afdca037a68c300d75922837
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-08 10:44:53 +0300

    Adding adjustments to the formulas

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
-        System.out.println(first > second && second > third ? "1. Greatest from 3": "1. Not greatest from 3");
+        System.out.println(first > second && second > third ? "1. Greatest from 3": (second > third || third > second));
     }
 }
 
